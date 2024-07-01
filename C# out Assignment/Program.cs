49c21efd6eea72b15bd01b2354bd01677cae6210
g.rithvik <g.rithvik909@gmail.com>
2024-07-01 12:58:48 +0530

    out assignment

---
+++ C# out Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+
+class out1
+{
+    public static void M1(out string s2)
+    {
+        Thread.Sleep(10000);
+        s2 = "rithvik";
+        Console.WriteLine("after intilization  " + s2);
+    }
+}
+
+class program : out1
+{
+    public static string s1 = "sf";
+
+    public Thread t1 = new Thread(() =>
+            { M1(out s1); })
+        { Name = "out thread" };
+
+    public void M()
+    {
+        t1.Start();
+        Console.WriteLine(s1);
+    }
+
+    public static void Main(string[] args)
+    {
+        program obj = new program();
+        obj.M();
+    }
+}
